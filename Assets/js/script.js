75fbcc0229c971a24e83b7a14fcd6c45ac6d3a62
Joe Curtis <joecurtis9999@gmail.com>
2022-02-02 20:39:54 +0000

    Added moment js to display date in DDDD DD MMMM YYYY format, media queries need to be added to make it responsive

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,2 +1,10 @@
 var apiKeyCMC = "2d0fde73-2204-4cb4-bffb-882e2f622e6b";
 var apiKeyND = "pub_421842bf54a3bf0ae27130d690ea3e6b77ef";
+
+var currentDate = moment().format('dddd') + " " + moment().format("Do MMM YYYY");
+
+
+var interval = setInterval(function () {
+    var momentNow = moment();
+    $('#currentDay').html(momentNow.format('dddd') + " " + momentNow.format('DD MMMM YYYY'));
+}, 100);
