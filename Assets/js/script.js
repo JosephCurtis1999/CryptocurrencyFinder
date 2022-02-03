e8940b03c8f773ae0f933eb64caefcb661304eae
Joe Curtis <joecurtis9999@gmail.com>
2022-02-03 18:33:55 +0000

    removed the set interval function no need for it

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,10 +1,11 @@
+// variables for API keys
 var apiKeyCMC = "2d0fde73-2204-4cb4-bffb-882e2f622e6b";
 var apiKeyND = "pub_421842bf54a3bf0ae27130d690ea3e6b77ef";
 
+// Shows date at the top of the page
 var currentDate =
   moment().format("dddd") + " " + moment().format("Do MMM YYYY");
 
-var interval = setInterval(function () {
-  var momentNow = moment();
+var momentNow = moment();
   $("#currentDay").html(momentNow.format("DD MMMM YYYY"));
-}, 100);
+
