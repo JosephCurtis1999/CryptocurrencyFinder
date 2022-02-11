a6ed4cac851d078e696bcedccae26f15d2c532e5
Joe Curtis <joecurtis9999@gmail.com>
2022-02-11 10:15:15 +0000

    Added a CryptoBasics Page with basic information for users, this is something that could be expanded on in the future to properly educate the user, perhaps a 'LEARN?' section, used some card flip animations like revision flashcards, made responsive and made it a separate page so users can go back and forth

---
+++ Assets/js/Cryptolearn.js
@@ -0,0 +1,35 @@
+const card = document.querySelector('.card__inner');
+
+card.addEventListener('click', function () {
+    card.classList.toggle('is-flipped');
+});
+
+const card2 = document.querySelector('.card__inner-2');
+
+card2.addEventListener('click', function () {
+    card2.classList.toggle('is-flipped');
+});
+
+const card3 = document.querySelector('.card__inner-3');
+
+card3.addEventListener('click', function () {
+    card3.classList.toggle('is-flipped');
+});
+
+const card4 = document.querySelector('.card__inner-4');
+
+card4.addEventListener('click', function () {
+    card4.classList.toggle('is-flipped');
+});
+
+const card5 = document.querySelector('.card__inner-5');
+
+card5.addEventListener('click', function () {
+    card5.classList.toggle('is-flipped');
+});
+
+const card6 = document.querySelector('.card__inner-6');
+
+card6.addEventListener('click', function () {
+    card6.classList.toggle('is-flipped');
+});
